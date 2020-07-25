348e96b9087307f84583a9c8addaaf2c877e6b9f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 01:01:03 +0530

    Created a basic form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <Form>
+        <label>Name</label>
+        <input type="text"></input>
+        <label>Job</label>
+        <input type="text"></input>
+      </Form>
+    )
+  }
+}
