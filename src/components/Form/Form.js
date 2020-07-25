514e244f0fac25ff67fa4a2120e6d689ab1ac9b7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 01:13:01 +0530

    Added input logic in Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Form extends Component {
+  state = {
+    name: '',
+    job: '',
+  };
+
+  nameCangeHandler = (event) => {
+    console.log('Name Change Handler');
+    const name = event.target.value;
+    this.setState({ name: name });
+  };
+
+  jobChangeHandler = (event) => {
+    console.log('Job Change Handler');
+    const job = event.target.value;
+    this.setState({ job: job });
+  };
+
+  onSubmitHandler = (event) => {
+    console.log('On Submit Handler');
+    if (this.state.name === '') alert('Name Cannot be Empty');
+    else if (this.state.job === '') alert('Job Cannot be Empty');
+  };
+
   render() {
     return (
-      <Form>
+      <form onSubmit={this.onSubmitHandler}>
         <label>Name</label>
-        <input type="text"></input>
+        <input type='text' onChange={this.nameCangeHandler}></input>
         <label>Job</label>
-        <input type="text"></input>
-      </Form>
-    )
+        <input type='text' onChange={this.jobChangeHandler}></input>
+        <button type='submit'>Add Employee</button>
+      </form>
+    );
   }
 }
