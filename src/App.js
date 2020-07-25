1f5bba72ac8784f22320828bc311c1d8c0d9c2ba
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 01:35:18 +0530

    Implemented Form Successfully sent form value to parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form, {  } from "./components/Form/Form";
+import Form from './components/Form/Form';
 
 class App extends Component {
-  render () {
+  state = {
+    name: '',
+    job: '',
+  };
+
+  submitValueHandler = (name, job) => {
+    this.setState({
+      name: name,
+      job: job,
+    });
+    console.log('Added Data' + this.state.name);
+  };
+
+  render() {
     return (
       <div>
-        <Form/>
+        <Form changeValue={this.submitValueHandler} />
       </div>
     );
   }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
-  state = {
-    name: '',
-    job: '',
-  };
-
-  nameCangeHandler = (event) => {
-    console.log('Name Change Handler');
-    const name = event.target.value;
-    this.setState({ name: name });
-  };
-
-  jobChangeHandler = (event) => {
-    console.log('Job Change Handler');
-    const job = event.target.value;
-    this.setState({ job: job });
-  };
-
+  
   onSubmitHandler = (event) => {
-    console.log('On Submit Handler');
-    if (this.state.name === '') alert('Name Cannot be Empty');
-    else if (this.state.job === '') alert('Job Cannot be Empty');
+    event.preventDefault();
+    if (event.target.name.value === '') alert('Name Cannot be Empty');
+    else if (event.target.job.value === '') alert('Job Cannot be Empty');
+    else {
+      this.props.changeValue(event.target.name.value, event.target.job.value);
+      event.target.name.value = '';
+      event.target.job.value = '';
+    }
   };
 
   render() {
     return (
       <form onSubmit={this.onSubmitHandler}>
         <label>Name</label>
-        <input type='text' onChange={this.nameCangeHandler}></input>
+        <input type='text' name='name'></input>
         <label>Job</label>
-        <input type='text' onChange={this.jobChangeHandler}></input>
+        <input type='text' name='job'></input>
         <button type='submit'>Add Employee</button>
       </form>
     );
